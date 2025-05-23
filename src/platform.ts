e1c1dbf158bc73c1ac4b10aa57b9cbfd791412bb
foxy85 <jfox05@gmail.com>
2025-05-23 19:38:33 +0100

    Update platform.ts
    This update ensures compatibility with Homebridge 2.0 and includes:
    
    Refactored class to use DynamicPlatformPlugin
    Implementation of configureAccessory
    Proper accessory discovery via didFinishLaunching

--- src/platform.ts
+++ src/platform.ts
@@ -1,320 +1,68 @@
+
 import {
   API,
-  CharacteristicEventTypes,
-  CharacteristicGetCallback,
-  CharacteristicSetCallback,
-  CharacteristicValue,
-  HAP,
-  IndependentPlatformPlugin,
-  Logging,
+  DynamicPlatformPlugin,
+  Logger,
+  PlatformAccessory,
   PlatformConfig,
+  Service,
+  Characteristic,
 } from 'homebridge';
 
-import { PLUGIN_NAME } from './settings';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 
 import SkyRemote = require('sky-remote');
 import SkyQCheck = require('sky-q');
-
-type ActiveCharacterstic = HAP['Characteristic']['Active']['INACTIVE'] | HAP['Characteristic']['Active']['ACTIVE'];
 
 interface SkyTVDeviceConfig {
   name?: string;
   ipAddress?: string;
 }
 
-interface SkyTVConfig extends PlatformConfig, SkyTVDeviceConfig {
+interface SkyTVConfig extends PlatformConfig {
   devices?: SkyTVDeviceConfig[];
 }
 
-export class SkyTVPlugin implements IndependentPlatformPlugin {
+export class SkyTVPlugin implements DynamicPlatformPlugin {
+  private readonly accessories: PlatformAccessory[] = [];
 
-  private readonly log: Logging;
-  private readonly api: API;
+  constructor(
+    private readonly log: Logger,
+    private readonly config: SkyTVConfig,
+    private readonly api: API,
+  ) {
+    this.log.debug('Finished initializing platform:', PLATFORM_NAME);
 
-  private names: string[] = [];
-
-  constructor(log: Logging, config: SkyTVConfig, api: API) {
-    this.log = log;
-    this.api = api;
-
-    if (config.devices) {
-      config.devices.forEach((deviceConfig, key) => {
-        deviceConfig = this.prepareDeviceConfig(key, deviceConfig);
-
-        this.publishExternalAccessory(deviceConfig);
-        this.names = [];
-      });
-    } else {
-      const deviceConfig = this.prepareDeviceConfig(null, config);
-
-      this.publishExternalAccessory(deviceConfig);
-    }
-
-    log.info('Sky TV platform finished initializing!');
+    this.api.on('didFinishLaunching', () => {
+      this.log.debug('Executed didFinishLaunching callback');
+      this.discoverDevices();
+    });
   }
 
-  prepareDeviceConfig(key: number | null, config: SkyTVDeviceConfig): SkyTVDeviceConfig {
-    if (!config.name) {
-      if (key === null) {
-        config.name = 'TV';
-      } else {
-        config.name = `TV ${key + 1}`;
-
-        if (this.names.includes(config.name)) {
-          this.log.error(`Duplicate name at device ${key + 1}.`);
-        }
-      }
-    }
-
-    if (!config.ipAddress) {
-      if (key === null) {
-        if (!config.ipAddress) {
-          this.log.error('IP address not set.');
-        }
-      } else {
-        if (!config.ipAddress) {
-          this.log.error(`IP address not set at device ${key + 1}.`);
-        }
-      }
-    }
-
-    return config;
+  configureAccessory(accessory: PlatformAccessory): void {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+    this.accessories.push(accessory);
   }
 
-  publishExternalAccessory(config: SkyTVDeviceConfig) {
-    if (!config.name) {
-      return;
-    }
-    if (!config.ipAddress) {
+  discoverDevices(): void {
+    if (!this.config.devices || !Array.isArray(this.config.devices)) {
+      this.log.warn('No devices configured');
       return;
     }
 
-    const remoteControl = new SkyRemote(config.ipAddress);
-    const boxCheck = new SkyQCheck({ ip: config.ipAddress });
+    for (const device of this.config.devices) {
+      const uuid = this.api.hap.uuid.generate(device.ipAddress ?? device.name ?? '');
+      const existingAccessory = this.accessories.find(acc => acc.UUID === uuid);
 
-    // Generate a UUID
-    const uuid = this.api.hap.uuid.generate(`homebridge:${PLUGIN_NAME}:` + config.ipAddress);
-
-    // Create the accessory
-    const accessory = new this.api.platformAccessory(config.name, uuid);
-
-    // Set the accessory category
-    accessory.category = this.api.hap.Categories.TV_SET_TOP_BOX;
-
-    // Add the accessory information service
-    const accessoryInfoService = accessory.getService(this.api.hap.Service.AccessoryInformation)!;
-    accessoryInfoService.setCharacteristic(this.api.hap.Characteristic.Manufacturer, 'Sky');
-
-    boxCheck._getSystemInformation({}).then(info => {
-      if (info.manufacturer) {
-        accessoryInfoService.setCharacteristic(this.api.hap.Characteristic.Manufacturer, info.manufacturer);
+      if (existingAccessory) {
+        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+        this.api.updatePlatformAccessories([existingAccessory]);
+      } else {
+        const accessory = new this.api.platformAccessory(device.name || 'Sky TV', uuid);
+        accessory.context.device = device;
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        this.log.info('Added new accessory:', accessory.displayName);
       }
-      
-      if (info.deviceType) {
-        accessoryInfoService.setCharacteristic(this.api.hap.Characteristic.Model, info.deviceType);
-      }
-
-      if (info.serialNumber) {
-        accessoryInfoService.setCharacteristic(this.api.hap.Characteristic.SerialNumber, info.serialNumber);
-      }
-    });
-
-    // Add the TV service
-    const tvService = accessory.addService(this.api.hap.Service.Television);
-
-    // Set the TV name
-    tvService.setCharacteristic(this.api.hap.Characteristic.ConfiguredName, config.name);
-
-    // Set sleep discovery characteristic
-    tvService.setCharacteristic(
-      this.api.hap.Characteristic.SleepDiscoveryMode,
-      this.api.hap.Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE,
-    );
-
-    boxCheck._request('as/services/favourites').then(favoritesData => {
-      if (favoritesData.favourites) {
-        boxCheck._request('as/services').then(servicesData => {
-          if (servicesData.services) {
-            tvService.getCharacteristic(this.api.hap.Characteristic.ActiveIdentifier)
-              .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-                const input = value.toString();
-
-                this.log.info(`[${config.name}]`, 'Set Input:', value);
-                this.send(remoteControl, [ 'backup', 'backup', 'backup', ...input ]).then(() => callback()).catch((error) => {
-                  this.log.error(error);
-                  callback(error);
-                });
-              });
-
-            favoritesData.favourites.forEach(favorite => {
-              const service = servicesData.services.filter(service => service.sid === favorite.sid)[0];
-
-              if (!service || !service.t || !service.c) {
-                return;
-              }
-    
-              const inputService = accessory.addService(this.api.hap.Service.InputSource, service.c, service.t);
-              inputService.setCharacteristic(this.api.hap.Characteristic.Identifier, parseInt(service.c));
-              inputService.setCharacteristic(this.api.hap.Characteristic.ConfiguredName, service.t);
-              inputService.setCharacteristic(this.api.hap.Characteristic.IsConfigured, this.api.hap.Characteristic.IsConfigured.CONFIGURED);
-              inputService.setCharacteristic(
-                this.api.hap.Characteristic.InputSourceType,
-                this.api.hap.Characteristic.InputSourceType.TUNER);
-              inputService.setCharacteristic(
-                this.api.hap.Characteristic.InputDeviceType,
-                this.api.hap.Characteristic.InputDeviceType.TUNER);
-    
-              tvService.addLinkedService(inputService);
-            });
-          }
-        });
-      }
-    });
-
-    // Handle on / off events using the active characteristic
-    tvService.getCharacteristic(this.api.hap.Characteristic.Active)
-      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-        this.getActive(boxCheck).then((activeState) => {
-          this.log.debug(`[${config.name}]`, 'Get Active: ' + (activeState ? 'ACTIVE': 'INACTIVE'));
-          callback(undefined, activeState);
-        }).catch((error) => {
-          this.log.error(`[${config.name}]`, 'Perhaps looking at this error will help you figure out why');
-          this.log.error(error);
-          callback(error);
-        });
-      })
-      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        this.getActive(boxCheck).then((activeState) => {
-          if (!!value === !!activeState) {
-            this.log.info(`[${config.name}]`, 'Skipping Active: new value is equal to current value');
-            return callback();
-          }
-  
-          this.log.info(`[${config.name}]`, 'Set Active: ' + (value ? 'ACTIVE': 'INACTIVE'));
-  
-          this.send(remoteControl, 'power').then(() => {
-            activeState = value ? this.api.hap.Characteristic.Active.ACTIVE : this.api.hap.Characteristic.Active.INACTIVE;
-            tvService.updateCharacteristic(this.api.hap.Characteristic.Active, activeState);
-            callback();
-          }).catch((error) => {
-            this.log.error(error);
-            callback(error);
-          });
-        }).catch((error) => {
-          this.log.error(`[${config.name}]`, 'Perhaps looking at this error will help you figure out why');
-          this.log.error(error);
-          callback(error);
-        });
-      },
-      );
-
-    // Handle remote control input
-    tvService.getCharacteristic(this.api.hap.Characteristic.RemoteKey)
-      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        const command: string | undefined = (() => {
-          switch (value) {
-            case this.api.hap.Characteristic.RemoteKey.REWIND:
-              return 'rewind';
-            case this.api.hap.Characteristic.RemoteKey.FAST_FORWARD:
-              return 'fastforward';
-            case this.api.hap.Characteristic.RemoteKey.ARROW_UP:
-              return 'up';
-            case this.api.hap.Characteristic.RemoteKey.ARROW_DOWN:
-              return 'down';
-            case this.api.hap.Characteristic.RemoteKey.ARROW_LEFT:
-              return 'left';
-            case this.api.hap.Characteristic.RemoteKey.ARROW_RIGHT:
-              return 'right';
-            case this.api.hap.Characteristic.RemoteKey.SELECT:
-              return 'select';
-            case this.api.hap.Characteristic.RemoteKey.BACK:
-              return 'backup';
-            case this.api.hap.Characteristic.RemoteKey.PLAY_PAUSE:
-              return 'play';
-            case this.api.hap.Characteristic.RemoteKey.INFORMATION:
-              return 'tvguide';
-            case this.api.hap.Characteristic.RemoteKey.EXIT:
-          }
-        })();
-
-        if (!command) {
-          this.log.error(`[${config.name}]`, `Skipping Remote Key: unknown new value: ${value}`);
-          return callback();
-        }
-        
-        this.log.info(`[${config.name}]`, 'Set Remote Key: ' + command);
-        this.send(remoteControl, command).then(() => callback()).catch((error) => {
-          this.log.error(error);
-          callback(error);
-        });
-      },
-      );
-
-    // Add the speaker service
-    const speakerService = accessory.addService(this.api.hap.Service.TelevisionSpeaker);
-    speakerService.setCharacteristic(this.api.hap.Characteristic.Active, this.api.hap.Characteristic.Active.ACTIVE);
-    speakerService.setCharacteristic(this.api.hap.Characteristic.VolumeControlType, this.api.hap.Characteristic.VolumeControlType.RELATIVE);
-
-    // Handle volume control
-    speakerService.getCharacteristic(this.api.hap.Characteristic.VolumeSelector)
-      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        const command = (() => {
-          switch (value) {
-            case this.api.hap.Characteristic.VolumeSelector.INCREMENT:
-              return 'channelup';
-            case this.api.hap.Characteristic.VolumeSelector.DECREMENT:
-              return 'channeldown';
-          }
-        })();
-
-        if (!command) {
-          this.log.error(`[${config.name}]`, `Skipping Volume Selector: unknown newValue: ${value}`);
-          return callback();
-        }
-        
-        this.log.info(`[${config.name}]`, 'Set Volume Selector: ' + command);
-        this.send(remoteControl, command).then(() => callback()).catch((error) => {
-          this.log.error(error);
-          callback(error);
-        });
-      },
-      );
-
-    // will be exposed as an additional accessory and must be paired separately with the pincode of homebridge
-    this.api.publishExternalAccessories(PLUGIN_NAME, [accessory]);
-
-    setInterval(() => {
-      this.getActive(boxCheck).then((activeState) => {
-        this.log.debug(`[${config.name}]`, 'Update Active: ' + (activeState ? 'ACTIVE': 'INACTIVE'));
-        tvService.updateCharacteristic(this.api.hap.Characteristic.Active, activeState);
-      }).catch((error) => {
-        this.log.error(`[${config.name}]`, 'Perhaps looking at this error will help you figure out why');
-        this.log.error(error);
-      });
-    }, 10000);
-  }
-
-  getActive = async (boxCheck: SkyQCheck): Promise<ActiveCharacterstic> => {
-    return new Promise<ActiveCharacterstic>((resolve, reject) => {
-      boxCheck.getPowerState().then(isOn => {
-        if (isOn) {
-          resolve(this.api.hap.Characteristic.Active.ACTIVE);
-        } else {
-          resolve(this.api.hap.Characteristic.Active.INACTIVE);
-        }
-      }).catch(error => {
-        this.log.error(error);
-        reject(error);
-      });
-    });
-  };
-
-  send = async (remoteControl: SkyRemote, command: string | string[]) => {
-    try {
-      remoteControl.press(command);
-    } catch (error) {
-      this.log.error(error);
-      return Promise.reject(error);
     }
-  };
+  }
 }
